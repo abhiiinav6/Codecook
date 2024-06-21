a88cfa407fd41d332bb73705918b437232249a88
Abhinav <abhinavkumar34002@gmail.com>
2024-06-21 15:15:05 +0530

    Init client

---
+++ client/src/main.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Signup from "./routes/Signup.tsx";
+import Login from "./routes/Login.tsx";
+
+const router = createBrowserRouter([
+    { path: "/", element: <App /> },
+    { path: "/ping", element: <h1>Pong</h1> },
+    { path: "/signup", element: <Signup /> },
+    { path: "/login", element: <Login /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
